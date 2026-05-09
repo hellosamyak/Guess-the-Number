a7da597fa141f1697d77a8dbec0efe30daef5486
hellosamyak <jainsamyak0805@gmail.com>
2026-05-09 17:34:44 +0530

    add all files

---
+++ script.js
@@ -0,0 +1,90 @@
+let randomNumber = parseInt(Math.random() * 100 + 1)
+
+const submit = document.querySelector('#submit')
+const userInput = document.querySelector('#guess-input')
+const prevGuesses = document.querySelector('#previous-guesses')
+const remainingAttempts = document.querySelector('#attempts-text')
+const hint = document.querySelector('#hint')
+
+let userGuesses = []
+let numOfAttempts = 1
+
+let playGame = true
+if (playGame) {
+  submit.addEventListener('click', function (e) {
+    e.preventDefault()
+    const guess = parseInt(userInput.value)
+    validateGuess(guess)
+  })
+
+}
+
+// initialize attempts display
+remainingAttempts.innerHTML = `${11 - numOfAttempts}`
+
+function validateGuess(guess) { // validates the inputs lie b/w 1 to 100 & number of attempts
+  if (isNaN(guess)) {
+    alert('Please enter a valid number!')
+  } else if (guess < 1) {
+    alert('Please enter a number greater than 1!')
+  } else if (guess > 100) {
+    alert('Please enter a number smaller than 100!')
+  } else {
+    userGuesses.push(guess)
+    if (numOfAttempts === 10) {
+      displayGuesses(guess)
+      displayMessage(`Game over. The number was ${randomNumber}`)
+      endGame()
+    } else {
+      displayGuesses(guess)
+      compareGuess(guess)
+    }
+  }
+}
+
+function compareGuess(guess) { // compares the input with the random number, tells equal, low or high
+  if (guess === randomNumber) {
+    displayMessage(`You guessed it right! YOU WONNNN!!!`)
+    userInput.setAttribute('disabled', '')
+  } else if (guess < randomNumber) {
+    displayMessage(`The number is too lowww`)
+  } else if (guess > randomNumber) {
+    displayMessage(`The number is too highhh`)
+  }
+}
+
+function displayGuesses(guess) { // cleans the input, updates the status panel
+  userInput.value = ''
+  prevGuesses.innerText += `${guess}, `
+  numOfAttempts++
+  remainingAttempts.innerHTML = `${11 - numOfAttempts}`
+}
+
+function displayMessage(message) { // passes a message, i.e., low or high
+  hint.innerHTML = `<h2>${message}</h2>`
+}
+
+function endGame() { // ends the game when guesses are over
+  userInput.value = ''
+  userInput.setAttribute('disabled', '')
+  playGame = false
+  StartNewGame()
+}
+
+function StartNewGame() { // starts a new game
+  const newGameBtn = document.querySelector('#new-game')
+  newGameBtn.addEventListener('click', function (e) {
+    randomNumber = parseInt(Math.random() * 100 + 1)
+    userGuesses = []
+    numOfAttempts = 1
+    prevGuesses.innerHTML = ''
+    remainingAttempts.innerHTML = `${11 - numOfAttempts}`
+    userInput.removeAttribute('disabled')
+    userInput.value = ''
+    hint.innerHTML = ''
+    playGame = true
+  })
+}
+
+// attach new-game listener on load so button works anytime
+StartNewGame()
